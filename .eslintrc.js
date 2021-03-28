328bdf91860a537817a89d0c56609ebac691860b
FernandoGOT <fernando.1f@hotmail.com>
2021-03-28 16:10:42 -0300

    ci(eslint and prettierrc): updated the lint and pretty settings
    Updated the pressier and eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'comma-dangle': ['error', 'never'],
+    'implicit-arrow-linebreak': ['error', 'beside'],
+    'max-len': ['error', { code: 140 }],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-props-no-spreading': [
       'error',
